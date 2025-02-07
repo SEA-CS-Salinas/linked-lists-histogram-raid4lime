257599468d0ec0a78a5942f88a15beafb623bb1e
KC <132471034+raid4lime@users.noreply.github.com>
2025-02-07 10:39:41 -0600

    2.0 HistoList.java

--- HistoList.java
+++ HistoList.java
@@ -1,4 +1,4 @@
-v//(c) A+ Computer Science
+//(c) A+ Computer Science
 //www.apluscompsci.com
 
 //Name - Casey Martinez
@@ -15,72 +15,73 @@ public class HistoList
 		front = null;
 	}
 
+	
 	//addLetter will add a new node to the front for let if let does not exist
 	//addLetter will bump up the count if let already exists
 	public void addLetter(char let)
 	{
-	    count = 0;
-	    while(front != let)
+	    HistoNode current = front;
+	    
+	    while(current != null) 
 	    {
-	        front = front.getNext();
-	    }
-	    boolean(char let == true)
-	    {
-	       count++; 
-	    }
-
-
-
-
-
-
-
-
+	         if (current.getLetter() == let) 
+	         { 
+                current.setLetterCount(current.getLetterCount() + 1);
+                return;
+            }
+            current = current.getNext();
+        }
+        front = new HistoNode(let, 1, front);
 	}
-
+	
 	//returns the index pos of let in the list if let exists
 	public int indexOf(char let)
 	{
-	    if( char let == true)
-	    {
-	        getLetterCount();
+	    HistoNode current = front;
+	    int count = 0;
+	    
+	   while(current != null)
+	   {
+	        if(current.getLetter() == let)
+	        {
+	            return count;
 	    }
-	    else(){
-		return null;
+        current = current.getNext();
+        count++;
 	    }
+	    return -1;
 	}
 
 	//returns a reference to the node at spot
 	private HistoNode nodeAt(int spot)
 	{
-		HistoNode current=null;
-
-
-
-
-
-
-
-
-
-
-		return current;
+		HistoNode current = front;
+		int count = 0;
+		
+		while(current != null)
+		{
+		    if(count == spot)
+		    {
+		        return current;
+		    }
+		    current = current.getNext();
+		    count++;
+		}
+		return null;
 	}
 
 	//returns a string will all values from list
 	public String toString()
 	{
-		String output = "";
 
-
-
-
-
-
-
-
-
-
+		String output = " ";
+		HistoNode current = front;
+		
+		while(current != null)
+		{
+		    output += current.getLetter() + " - "  + current.getLetterCount()+ "  ";
+		    current = current.getNext();
+		}
 		return output;
 	}
 }
