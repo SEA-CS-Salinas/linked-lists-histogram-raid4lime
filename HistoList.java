3379edae2e7d309eebc086f1de39b50fd84375fa
KC <132471034+raid4lime@users.noreply.github.com>
2025-02-07 10:51:14 -0600

    3.0, Fin HistoList.java

--- HistoList.java
+++ HistoList.java
@@ -8,11 +8,11 @@ import static java.lang.System.*;
 
 public class HistoList
 {
-   private HistoNode front;
+   private HistoNode front; // Front of node
 
 	public HistoList( )
 	{
-		front = null;
+		front = null; // No nodes to begin with
 	}
 
 	
@@ -20,68 +20,72 @@ public class HistoList
 	//addLetter will bump up the count if let already exists
 	public void addLetter(char let)
 	{
-	    HistoNode current = front;
-	    
+	    HistoNode current = front; // Start with front of list
+
+	    // Looks through list to see if letter already exits
 	    while(current != null) 
 	    {
 	         if (current.getLetter() == let) 
 	         { 
-                current.setLetterCount(current.getLetterCount() + 1);
+                current.setLetterCount(current.getLetterCount() + 1); // If letter does exist it will add to the count and return
                 return;
             }
             current = current.getNext();
         }
-        front = new HistoNode(let, 1, front);
+        front = new HistoNode(let, 1, front); // Shifts to next node
 	}
 	
 	//returns the index pos of let in the list if let exists
 	public int indexOf(char let)
 	{
 	    HistoNode current = front;
-	    int count = 0;
-	    
+	    int count = 0; // used to track node 
+
+	   // Used to look through node
 	   while(current != null)
 	   {
 	        if(current.getLetter() == let)
 	        {
-	            return count;
+	            return count; // If letter is found returns the count
 	    }
-        current = current.getNext();
-        count++;
+        current = current.getNext(); // Shifts to next node
+        count++; // +1 to count
 	    }
-	    return -1;
+	    return -1; // returns -1 if no Letter is found
 	}
 
 	//returns a reference to the node at spot
 	private HistoNode nodeAt(int spot)
 	{
-		HistoNode current = front;
-		int count = 0;
-		
+		HistoNode current = front; // Start of current node
+		int count = 0; // Track count of current node
+
+		// Looks through list until spot is found
 		while(current != null)
 		{
-		    if(count == spot)
+		    if(count == spot) 
 		    {
-		        return current;
+		        return current; // if spot is found returns the current node
 		    }
-		    current = current.getNext();
-		    count++;
+		    current = current.getNext(); // Shifts to next node
+		    count++; // Adds to count
 		}
-		return null;
+		return null; // Returns null if out of bounds
 	}
 
 	//returns a string will all values from list
 	public String toString()
 	{
 
-		String output = " ";
+		String output = " "; 
 		HistoNode current = front;
-		
+
+		// As long as the node isn't empty it will keep iterating, used to print out the sample output
 		while(current != null)
 		{
-		    output += current.getLetter() + " - "  + current.getLetterCount()+ "  ";
-		    current = current.getNext();
+		    output += current.getLetter() + " - "  + current.getLetterCount()+ "  "; // Formating
+		    current = current.getNext(); // Shifts to next node
 		}
-		return output;
+		return output; // Returns the String
 	}
 }
